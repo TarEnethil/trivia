6a2a877bd0c6d6e3901b9ae6ba33dba3f79689d5
Thorben Römer <thorbenroemer@t-online.de>
2023-05-19 16:46:46 +0200

    fix: correct path in cron.sh
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 
 URL="localhost:8337"
 DIR=$(dirname $0)
-TOKEN=$(sqlite3 $DIR/app/app.db "SELECT bot_token FROM general_settings WHERE id = 1")
+TOKEN=$(sqlite3 $DIR/data/app.db "SELECT bot_token FROM general_settings WHERE id = 1")
 
 curl $URL/bot/publish?token=$TOKEN
